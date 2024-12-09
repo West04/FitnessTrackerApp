abb454547bcb7ab101c53287fee7fed863acb634
West04 <west@finleysound.com>
2024-12-08 20:11:53 -0800

    The Beginning of the Login Page

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import streamlit as st
 from dotenv import load_dotenv
+from supabase import create_client, Client
 import os
 
-load_dotenv()
 
-testing = os.environ.get('TEST')
+def main():
+    # Load Environment Variables
+    load_dotenv()
 
-print(testing)
-print(testing)
+    # Connect to Supabase
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+    supabase: Client = create_client(url, key)
+
+    st.title("Log In")
+    st.text_input("Enter your email", key="supabase_email")
+    st.text_input("Enter your password", key="supabase_password")
+
+    print(st.session_state.supabase_email, st.session_state.supabase_password)
+
+
+if __name__ == '__main__':
+    main()
