e1927a21be6c98f432040472d382c4d84f8692b6
West04 <west@finleysound.com>
2024-12-08 22:42:45 -0800

    The Beginning of the Login Page

--- main.py
+++ main.py
@@ -13,11 +13,17 @@ def main():
     key: str = os.environ.get("SUPABASE_KEY")
     supabase: Client = create_client(url, key)
 
-    st.title("Log In")
-    st.text_input("Enter your email", key="supabase_email")
-    st.text_input("Enter your password", key="supabase_password")
+    # Create a container to center the content
+    col1, col2, col3 = st.columns([1, 2, 1])
 
-    print(st.session_state.supabase_email, st.session_state.supabase_password)
+    with col2:
+        st.title("Log In")
+        st.text_input("Enter your email", key="supabase_email")
+        st.text_input("Enter your password", key="supabase_password")
+
+        if 'supabase_email' in st.session_state and 'supabase_password' in st.session_state:
+            if st.button("Log In") and st.session_state.supabase_email and st.session_state.supabase_password:
+                print(st.session_state.supabase_email, st.session_state.supabase_password)
 
 
 if __name__ == '__main__':
