9b0cedf6527eff271be2c30fee6faccdd3c27f8c
West04 <west@finleysound.com>
2024-12-10 10:23:55 -0800

    Starting for backend for Login page

--- main.py
+++ main.py
@@ -8,22 +8,60 @@ def main():
     # Load Environment Variables
     load_dotenv()
 
-    # Connect to Supabase
+    # Connect to Supabase Client
     url: str = os.environ.get("SUPABASE_URL")
     key: str = os.environ.get("SUPABASE_KEY")
     supabase: Client = create_client(url, key)
 
-    # Create a container to center the content
     col1, col2, col3 = st.columns([1, 2, 1])
 
     with col2:
-        st.title("Log In")
-        st.text_input("Enter your email", key="supabase_email")
-        st.text_input("Enter your password", key="supabase_password")
+        auth_mode = st.radio("Login or Sign Up?", ("Login", "Sign Up"))
 
-        if 'supabase_email' in st.session_state and 'supabase_password' in st.session_state:
-            if st.button("Log In") and st.session_state.supabase_email and st.session_state.supabase_password:
-                print(st.session_state.supabase_email, st.session_state.supabase_password)
+        if auth_mode == "Login":
+            st.title("Log In")
+            email = st.text_input("Enter your email")
+            password = st.text_input("Enter your password")
+
+            # if login(supabase, email, password):
+            #    st.switch_page("home.py")
+
+        else:
+            st.title("Log In")
+            email = st.text_input("Enter your email")
+            password1 = st.text_input("Enter your password")
+            password2 = st.text_input("Confirm your password")
+
+            # if password1 == password2:
+            #    signup(supabase, email, password1)
+            # else:
+            #    st.error("Passwords don't match")
+
+
+def login(supabase: Client, email: str, password: str):
+    try:
+        response = supabase.auth.sign_in_with_password({
+            "email": email,
+            "password": password
+        })
+        return response.session is not None
+    except Exception as e:
+        st.error(f"Login failed: {str(e)}")
+        return False
+
+
+def signup(supabase: Client, email: str, password: str):
+    try:
+        response = supabase.auth.sign_up({
+            "email": email,
+            "password": password
+        })
+        st.success("Account created successful")
+        return response.session is not None
+
+    except Exception as e:
+        st.error(f"Sign up failed: {str(e)}")
+        return False
 
 
 if __name__ == '__main__':
